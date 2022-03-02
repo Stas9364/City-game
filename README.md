9201d6cbe0cf91a6d62dd83b6b4433f3da2ad5cd
Stas9364 <86848801+Stas9364@users.noreply.github.com>
2022-03-02 05:18:45 +0300

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # City-game
 Game for two person
+City can be use one time and next city must start with last letter of the first city 
---
+++ script.js
@@ -0,0 +1,40 @@
+let inp = document.querySelector('.field');
+let out = document.querySelector('.out-city');
+
+inp.addEventListener('keypress', function (event) {
+    if (event.code == "Enter") check ();
+});
+
+let arr = [];
+
+
+function check () {
+    let x = inp.value.toUpperCase();
+
+    if (arr.length == 0) {
+        arr.push(x);
+        out.innerHTML = arr[arr.length-1].toUpperCase();
+        inp.value = '';
+    }else if (arr.includes(x)){
+        out.innerHTML = `${x.toUpperCase()} has already been`;
+        inp.value = '';
+    }else if (lastLetter() == x[0]){
+        arr.push(x);
+        out.innerHTML = arr[arr.length-1];
+        inp.value = '';
+    }else {
+        out.innerHTML = `You should start with ${lastLetter().toUpperCase()}`;
+        inp.value = '';
+    }
+
+    lastLetter();
+}
+
+
+function lastLetter () {
+    let lastWord = arr[arr.length-1];
+    // let o = lastWord.split('');
+    // let last = o[o.length-1]
+    let lastL = lastWord[lastWord.length-1]
+    return lastL;
+}
