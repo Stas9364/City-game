8b2d3b01e06711abff4fac2eb6c3c8aa3968c585
Stas9364 <stas-lisovskii@mail.ru>
2022-03-03 20:13:30 +0300

    Get changes

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let inp = document.querySelector('.field');
 let out = document.querySelector('.out-city');
 
@@ -31,7 +30,6 @@ function check () {
     lastLetter();
 }
 
-
 function lastLetter () {
     let lastWord = arr[arr.length-1];
     // let o = lastWord.split('');
@@ -39,43 +37,3 @@ function lastLetter () {
     let lastL = lastWord[lastWord.length-1]
     return lastL;
 }
-=======
-let inp = document.querySelector('.field');
-let out = document.querySelector('.out-city');
-
-inp.addEventListener('keypress', function (event) {
-    if (event.code == "Enter") check ();
-});
-
-let arr = [];
-
-function check () {
-    let x = inp.value.toLowerCase();
-
-
-    // if (arr.length == 0) {
-    //     arr.push(x);
-    // }
-    
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] == inp) {
-            console.log('This word has already been')
-    }else {
-            arr.push(x);
-        }
-    }
-
-
-
-
-    inp.value = '';
-    console.log(arr)
-
-
-
-
-}
-
-
-
->>>>>>> c56d18e3b138441edf1d5e07be7946aacf6f925c
